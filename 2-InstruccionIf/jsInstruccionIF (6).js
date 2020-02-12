3054913a0416d7304bc2552c631751700860f945
alumno <alumno>
2020-02-12 12:18:45 -0300

    clase 3

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if(edad <= 13){
+
+    alert("Niño");
+  }
+ else if(edad <= 17){
+     alert("adolescente");
+ }
+ else{
+     alert("adulto");
+ }
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estado;
 
-	
+edad = parseInt(document.getElementById("edad").value);
+estado = document.getElementById("estadoCivil").value;
+
+if (edad <=17 && estado != "Soltero"){
+
+    alert("Es muy joven para no ser soltero");
+}
 
 
 }//FIN DE LA FUNCIÓN
